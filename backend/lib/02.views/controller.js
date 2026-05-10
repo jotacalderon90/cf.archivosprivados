66d1d49320e5b8dd1ae56a5227c20b0954aad55f
Jhonatan Calderon <jcalderon.aprog@gmail.com>
2026-05-10 01:01:42 -0400

    mejora

--- backend/lib/02.views/controller.js
+++ backend/lib/02.views/controller.js
@@ -4,11 +4,22 @@ const logger = require('cl.jotacalderon.cf.framework/lib/log')(__filename);
 const response = require('cl.jotacalderon.cf.framework/lib/response');
 const constants = require('./constants');
 
+const getHostAccount = function (req) {
+  let hostAccount = '';
+  if (process.env.NODE_ENV === 'production' && process.env.FRONT_MULTIDOMAIN === '1') {
+    hostAccount = req.protocol + '://' + req.headers.host.replace(/^([^.:]+)/, 'account');
+  } else {
+    hostAccount = process.env.HOST_ACCOUNT;
+  }
+  return hostAccount;
+};
+
 module.exports = {
   index: async function (req, res) {
     try {
       res.render('filemanager/_', {
         user: req.user,
+        __hostAccount: getHostAccount(req),
       });
     } catch (error) {
       logger.error(error);
