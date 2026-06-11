6c263839e59e442d7702e4fa9705c3a1d55ba5a9
Jhonatan Calderon <jcalderon.aprog@gmail.com>
2026-06-11 13:08:24 -0400

    mejora directorio

--- backend/lib/07.fulldirectory/service.js
+++ backend/lib/07.fulldirectory/service.js
@@ -7,33 +7,37 @@ const logger = require('cl.jotacalderon.cf.framework/lib/log')(__filename);
 
 const constants = require('./constants');
 
-const directory = process.cwd() + '/frontend/assets/';
+const directory = process.cwd() + '/backend/assets/';
 
-const getDirectory = function(src, dirbase){
+const getDirectory = function (src, dirbase) {
   const tmpDir = fs.readdirSync(src);
-	const directory = [];
-	for(let i=0;i<tmpDir.length;i++){
+  const directory = [];
+  for (let i = 0; i < tmpDir.length; i++) {
     const direct = path.join(src, tmpDir[i]);
-		const dir = {text: tmpDir[i], id: dirbase + tmpDir[i], type: (fs.statSync(direct).isDirectory())?'folder':'file'}
-		if(fs.statSync(direct).isDirectory()){
-      dir.children = getDirectory(direct, dirbase + tmpDir[i] + "/");
+    const dir = {
+      text: tmpDir[i],
+      id: dirbase + tmpDir[i],
+      type: fs.statSync(direct).isDirectory() ? 'folder' : 'file',
+    };
+    if (fs.statSync(direct).isDirectory()) {
+      dir.children = getDirectory(direct, dirbase + tmpDir[i] + '/');
     }
-		directory.push(dir);
+    directory.push(dir);
   }
-	return directory;
+  return directory;
 };
-      
+
 module.exports = {
-  
-  fulldirectory: async function() {
-    try{
-			
+  fulldirectory: async function () {
+    try {
       return getDirectory(directory, '/');
-      
-		}catch(error){
-			logger.error(error);
-      throw new Error((error instanceof Error) ? error.message : constants.error.rest.fulldirectory + ' ' + constants.error.servicio);
-		}
-  }
-  
-}
+    } catch (error) {
+      logger.error(error);
+      throw new Error(
+        error instanceof Error
+          ? error.message
+          : constants.error.rest.fulldirectory + ' ' + constants.error.servicio
+      );
+    }
+  },
+};
